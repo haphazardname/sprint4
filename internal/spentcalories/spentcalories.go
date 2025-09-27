295f8a9069073d000a68fcae64b69e6cb0bd5956
Olga Sokolova <sokolova.o.yu@gmail.com>
2025-09-27 18:19:13 +0300

    1st

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -11,7 +11,7 @@ import (
 
 // Основные константы, необходимые для расчетов.
 const (
-	lenStep                    = 0.65 // средняя длина шага.
+	//lenStep                    = 0.65 // средняя длина шага.
 	mInKm                      = 1000 // количество метров в километре.
 	minInH                     = 60   // количество минут в часе.
 	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
@@ -69,7 +69,7 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 		return "", err
 	}
 
-	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %d ч.\nДистанция: %d км.\nСкорость: %d км/ч\nСожгли калорий: %d", typeTrain, trainDur, distance(steps, height), meanSpeed(steps, height, trainDur), calories), nil
+	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f", typeTrain, trainDur.Hours(), distance(steps, height), meanSpeed(steps, height, trainDur), calories), nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
