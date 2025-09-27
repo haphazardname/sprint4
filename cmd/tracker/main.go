2ae032d8feb676303b70c048f878c3976fa8271a
Olga Sokolova <sokolova.o.yu@gmail.com>
2025-09-27 23:19:13 +0300

    %)

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"log"
 
-	//"github.com/Yandex-Practicum/tracker/internal/daysteps"
-	//"github.com/Yandex-Practicum/tracker/internal/spentcalories"
-
-	"internal/daysteps"
-	"internal/spentcalories"
+	"github.com/Yandex-Practicum/tracker/internal/daysteps"
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 func main() {
