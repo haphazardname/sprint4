5d620d532f3a7031aa9937a17cca7ff020273c60
Olga Sokolova <sokolova.o.yu@gmail.com>
2025-10-01 23:28:43 +0300

    проверки на валижность

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -3,6 +3,7 @@ package daysteps
 import (
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -20,31 +21,38 @@ const (
 func parsePackage(data string) (int, time.Duration, error) {
 	dataSlice := strings.Split(data, ",")
 	if len(dataSlice) != 2 {
-		return 0, 0, errors.New("Неверный формат входных данных parsePackage")
+		err := errors.New("неверный формат входных данных parsePackage")
+		log.Println(err)
+		return 0, 0, err
 	}
 
 	steps, err := strconv.Atoi(dataSlice[0])
 	if err != nil || steps <= 0 {
-		return 0, 0, errors.New("Ошибка преобразования шагов: " + err.Error())
+		err := errors.New("ошибка преобразования шагов parsePackage")
+		log.Println(err)
+		return 0, 0, err
 	}
 
 	trainDur, err := time.ParseDuration(dataSlice[1])
-	if err != nil {
-
-		return 0, 0, errors.New("Ошибка преобразования длительности тренировки: " + err.Error())
+	if err != nil || trainDur.Seconds() <= 0 {
+		err := errors.New("ошибка преобразования длительности тренировки parsePackage")
+		log.Println(err)
+		return 0, 0, err
 	}
 	return steps, trainDur, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
 	steps, trainDur, err := parsePackage(data)
-	if err != nil || steps <= 0 {
+	if err != nil || steps <= 0 || trainDur.Seconds() <= 0 {
+		log.Println(err)
 		return ""
 	}
-	distance := (stepLength * float64(steps)) / mInKm
+	distance := (stepLength * float64(steps)) / float64(mInKm)
 	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, trainDur)
 	if err != nil {
+		log.Println(err)
 		return ""
 	}
-	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", steps, distance, calories)
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, calories)
 }
