c2dee655879bd6d32392d5f453849a15a60e2c46
Olga Sokolova <sokolova.o.yu@gmail.com>
2025-10-01 23:31:27 +0300

    test valid

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -55,4 +55,5 @@ func DayActionInfo(data string, weight, height float64) string {
 		return ""
 	}
 	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, calories)
+
 }
